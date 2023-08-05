c192179e7eb1d613a82558da2ebd0983cccc41d2
lemstanislav5 <lemstanislav5@gmail.com>
2023-08-05 14:19:01 +0300

    121

--- services/dataBaseSqlite3.js
+++ services/dataBaseSqlite3.js
@@ -1,3 +1,7 @@
+/**
+ * Основной принцип - избежать создания лишних сущностей (таблиц, объектов)
+ * ?Нужно расмотреть вопрос о необходимости добавления в таблицу "clients" полей phone и  email, либо создания отдельной таблицы?
+*/
 const sqlite3 = require('sqlite3').verbose();
 const query = (file, req, sql, params = []) => {
     return new Promise((resolve, reject) => {
@@ -23,24 +27,25 @@ const query = (file, req, sql, params = []) => {
 module.exports = {
     databaseInitialization: () => {
         return Promise.all([
-            query('data.db3', 'run', "CREATE TABLE if not exists `users` (`id` INTEGER PRIMARY KEY AUTOINCREMENT,  `chatId` TEXT, `socketId` TEXT, `name` TEXT, `online` INTEGER)"),
+            query('data.db3', 'run', "CREATE TABLE if not exists `clients` (`id` INTEGER PRIMARY KEY AUTOINCREMENT,  `chatId` TEXT, `socketId` TEXT, `name` TEXT, `online` INTEGER)"),
             query('data.db3', 'run', "CREATE TABLE if not exists `messeges` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `chatId` TEXT,`socketId` TEXT, `messageId` TEXT, `text` TEXT, `time`  INTEGER, `type` TEXT, `read` INTEGER)"),
-            query('data.db3', 'run', "CREATE TABLE if not exists `manager` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `chatId` TEXT,`socketId` TEXT, `name` TEXT, `password` TEXT)"),
+            query('data.db3', 'run', "CREATE TABLE if not exists `manager` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `chatId` TEXT,`socketId` TEXT, `name` TEXT, `password` TEXT, `email` TEXT)"),
         ])
     },
-    //USERS
-    addUser: (chatId, socketId) => (query('data.db3', 'run', 'INSERT INTO users (chatId, socketId) values ("' + chatId + '","' + socketId + '")', [])),
+    //clients
+    addUser: (chatId, socketId) => (query('data.db3', 'run', 'INSERT INTO clients (chatId, socketId) values ("' + chatId + '","' + socketId + '")', [])),
     addMessage: (chatId, socketId, messageId, text, time, type, read) => (query('data.db3', 'run', 'INSERT INTO messeges (chatId, socketId, messageId, text, time, type, read) values ("' +
     chatId + '","' + socketId + '","' + messageId + '","' + text + '","' + time + '","' + type + '","' + read + '")', [])),
-    findUser: (chatId) => (query('data.db3', 'all', 'SELECT * FROM users WHERE chatId = "' + chatId + '"', [])),
-    updateSocketId: (chatId, socketId) => (query('data.db3', 'run', 'UPDATE users SET socketId=? WHERE chatId=?', [socketId, chatId])),
-    getAllUsers: () => (query('data.db3', 'all', 'SELECT * FROM users', [])),
+    findUser: (chatId) => (query('data.db3', 'all', 'SELECT * FROM clients WHERE chatId = "' + chatId + '"', [])),
+    updateSocketId: (chatId, socketId) => (query('data.db3', 'run', 'UPDATE clients SET socketId=? WHERE chatId=?', [socketId, chatId])),
+    getAllclients: () => (query('data.db3', 'all', 'SELECT * FROM clients', [])),
     //! Выбор число непрочитанных сообщений
     getMesseges: () => (query('data.db3', 'all', 'SELECT * FROM messeges', [])),
     updateCurrentUser: (chatId) => (query('data.db3', 'run', 'UPDATE currentUser SET chatId=?', [chatId])),
-    userOnline: (socketId) => (query('data.db3', 'run', 'UPDATE users SET online=? WHERE socketId=?', [1, socketId])),
-    userOffline: (socketId) => (query('data.db3', 'run', 'UPDATE users SET online=? WHERE socketId=?', [0, socketId])),
-    findUserBySocketId: (socketId) => (query('data.db3', 'all', 'SELECT * FROM users WHERE socketId = "' + socketId + '"', [])),
+    userOnline: (socketId) => (query('data.db3', 'run', 'UPDATE clients SET online=? WHERE socketId=?', [1, socketId])),
+    userOffline: (socketId) => (query('data.db3', 'run', 'UPDATE clients SET online=? WHERE socketId=?', [0, socketId])),
+    findUserBySocketId: (socketId) => (query('data.db3', 'all', 'SELECT * FROM clients WHERE socketId = "' + socketId + '"', [])),
     //MANAGER
-    getManager:() => (query('data.db3', 'all', 'SELECT * FROM manager', [])),
+    addManager: () => (query('data.db3', 'all', 'INSERT INTO clients (chatId, socketId, name, password, email) values ("' + chatId + '","' + socketId + '")', [])),
+    getManager: () => (query('data.db3', 'all', 'SELECT * FROM manager', [])),
 }
