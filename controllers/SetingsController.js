7cb1cd34b60700cf41330dba2e4c2f2c2c39c841
lemstanislav5 <lemstanislav5@gmail.com>
2024-01-08 18:53:57 +0300

    SocketSection

--- controllers/SetingsController.js
+++ controllers/SetingsController.js
@@ -8,21 +8,28 @@ const {
 
 class SetingsController {
   async get(){
-    const toArr = (obj) => {
-      const arr = [];
-        for (var key in obj[0]) {
-          console.log(typeof obj[0][key], obj[0][key])
-          if (obj[0].hasOwnProperty(key) && key !== 'id') arr.push([key, obj[0][key]]);
-        }
-      return arr;
+    // const toArr = (obj) => {
+    //   const arr = [];
+    //     for (var key in obj[0]) {
+    //       console.log(typeof obj[0][key], obj[0][key])
+    //       if (obj[0].hasOwnProperty(key) && key !== 'id') arr.push([key, obj[0][key]]);
+    //     }
+    //   return arr;
+    // }
+    // const setings = [
+    //   ['socket', toArr(await getSetingsSocketUser())],
+    //   ['consent', toArr(await getSetingsConsentUser())],
+    //   ['colors', toArr(await getSetingsColorsUser())],
+    //   ['questions', toArr(await getSetingQuestionsUser())],
+    //   ['contact', toArr(await getSetingsContactUser())],
+    // ]
+    const setings = {
+      socket: Object.assign({}, await getSetingsSocketUser()),
+      consent: Object.assign({}, await getSetingsConsentUser()),
+      colors: await getSetingsColorsUser(),
+      questions: Object.assign({}, await getSetingQuestionsUser()),
+      contact: Object.assign({}, await getSetingsContactUser()),
     }
-    const setings = [
-      ['socket', toArr(await getSetingsSocketUser())],
-      ['consent', toArr(await getSetingsConsentUser())],
-      ['colors', toArr(await getSetingsColorsUser())],
-      ['questions', toArr(await getSetingQuestionsUser())],
-      ['contact', toArr(await getSetingsContactUser())],
-    ]
     log(__filename, 'Настройки пользователя');
     table(setings);
     return setings;
